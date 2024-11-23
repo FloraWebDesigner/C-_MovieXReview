bb38605fc15efa4a344b8b9671475d3bc98fd8d2
suzanna8767 <codessuzy@gmail.com>
2024-11-23 18:48:18 -0500

    changed folder migration location

--- MovieXReview/obj/Debug/net8.0/MovieXReview.AssemblyInfo.cs
+++ MovieXReview/obj/Debug/net8.0/MovieXReview.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieXReview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bdb8b2ca66ffdbd8cc1d41e0a401e163ae705a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8798f967766920d2b549ddbafe01abd5265803d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieXReview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieXReview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
